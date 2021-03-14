b378c576bc1c279aad4ff6f3239eb4dc573b6c4a
Sejal Shah <sejal_shah@brown.edu>
2021-03-14 16:31:59 -0400

    pure lit elim

---
+++ pure_lit_elim.py
@@ -0,0 +1,23 @@
+from classes import Literal, Clause
+from general import update_assignments, remove_clauses_with_literal, negate_literal
+from typing import Callable, List, Set, Tuple
+
+def find_pure(inst : Set[Clause], lits : Set[Literal]):
+    for l in lits:
+        pure = True
+        for c in inst:
+            (negate_literal(l) not in c) and pure
+        if not pure:
+            continue
+        else:
+            return l
+            break
+
+def pure_literal_elim(inst : Set[Clause], assigned : Set[Literal], unassigned : Set[Literal])
+    elim = find_pure(inst, unassigned)
+    if elim == None:
+        return inst
+    else:
+        update_assignments(assigned, unassigned, elim)
+        new_inst = remove_clauses_with_literal(inst, elim)
+        return pure_literal_elim(new_inst, assigned, unassigned)
