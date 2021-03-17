44be6e0343b96eb6494fe3f3736e2ef5261badaf
Sreshtaa Rajesh <teta.rajesh@gmail.com>
2021-03-17 17:54:10 -0400

    made preliminary testing script

---
+++ testing_script.py
@@ -0,0 +1,52 @@
+import sys
+from copy import copy, deepcopy
+import random 
+from typing import Callable, List, Set, Tuple
+from classes import Literal, Clause
+from solver import readInput, solve
+
+def generate_literals(num_lits : int):
+    lit_set = set()
+    for i in range(1, num_lits): 
+        lit_set.add(Literal(i, True))
+        lit_set.add(Literal(i, False))
+    
+    return lit_set
+
+def generate_clauses(set_lits : Set[Literal]): 
+    set_clauses = set()
+
+    num_clauses = random.randrange(2, len(set_lits) + 4)
+    for i in range(1, num_clauses): 
+        clause_size = random.randrange(1, len(set_lits))
+        lits_in_clause = set()
+        for j in range(1, clause_size): 
+            lits_copy = set_lits.copy()
+            lits_in_clause.add(lits_copy.pop())
+        
+        set_clauses.add(Clause(i, lits_in_clause))
+    
+    return set_clauses
+    
+
+def check_validity_clauses(assignment : Set[Literal], clauses : Set[Clause]): 
+    for clause in clauses: 
+        if assignment.intersection(clause.literal_set) == set(): 
+            return False
+    
+    return True
+
+def main(): 
+    inputFile = sys.argv[1]
+    assigned, unassigned, clause_set = readInput(inputFile)
+    assignment = solve(assigned, unassigned, clause_set)
+    
+    if assignment == set(): 
+        print("UNSAT")
+    else: 
+        print("Our assignment: " + str(assignment))
+        print("Satisfies: " + str(check_validity_clauses(assignment, clause_set)))
+    return 
+
+if __name__ == "__main__":
+    main()
